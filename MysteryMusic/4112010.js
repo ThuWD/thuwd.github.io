03cdb1c3afb42b0316d5153b64f849b104f2302e
ThuWD <61614127+ThuWD@users.noreply.github.com>
2020-05-13 12:11:06 +1000

    Add files via upload

---
+++ MysteryMusic/4112010.js
@@ -0,0 +1,13 @@
+var answer=prompt("This is a quiz. If you do not want to participate then type no. If yes, type yes.");
+if(answer=="no") alert("There's no backing out now...");
+else alert("Hey!");
+if(answer=="yes") alert("LET'S DO THIS!");
+else alert("Hey!");
+
+var answer=prompt("What is my full name?");
+if(answer=="Tim Dowling") alert("Nice... Ollie, Max");
+else alert("Heh...");
+
+var answer=prompt("What year did I make this?");
+if(answer=="2019") alert("Good job, bro");
+else alert("You could have checked that on the chrome web store y'know");
---
+++ MysteryMusic/popup.js
@@ -0,0 +1,13 @@
+console.log("HEY! nothing to see here...");
+
+var Msg = "Hey! What are you doing?"
+
+function cmd() {
+	return(Msg);
+};
+setTimeout(function(){ alert("15 FRICKIN MINUTES, DUDE. OH YEAH!"); }, 900000);
+
+function broken() {
+	alert("how in the crap did you find this?");
+	document.write("Broken...");
+};
