227d481d7cfdd1cb5b244cc92aac266521d894e3
ThuWD Kanye <61614127+ThuWD@users.noreply.github.com>
2020-05-18 11:11:32 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function style() {
 document.getElementById("Honorable-mentions").style["color"] = "white";
 document.getElementById("Honorable-mentions").style["font-family"] = "Sulphur Point";
 }
-window.onload = style();
+setInterval(style, );
